f5a745e60d5bdc9e5350c3fc0ed6d20af4fa3aff
GamerFoxy12 <pogyproguy@gmail.com>
2026-06-30 15:34:49 +0300

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,9 +1,5 @@
-if not isfolder("sentinellite") then
-    makefolder("sentinellite")
-end
-if isfolder("sentinellite") and not isfile("sentinellite/main.lua") then
-    writefile("sentinellite/main.lua", game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/SentinelLite/refs/heads/main/main.lua", true))
-    loadstring(readfile("sentinellite/main.lua"))()
-elseif isfolder("sentinellite") and isfile("sentinellite/main.lua") then
-    loadstring(readfile("sentinellite/main.lua"))()
-end
+local d = "sentinellite"
+local u = "SentinelLite"
+local t, l = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/"..u.."/refs/heads/main/version.txt"):gsub("%s+", "") end), isfile(d.."/version.cfg") and readfile(d.."/version.cfg"):gsub("%s+", "") or ""
+if not t or l ~= game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/"..u.."/refs/heads/main/version.txt"):gsub("%s+", "") or not isfile(d.."/main.lua") then if isfolder(d) then pcall(delfolder, d.."/games") pcall(delfolder, d.."/guis") pcall(delfile, d.."/main.lua") end if not isfolder(d) then makefolder(d) end writefile(d.."/main.lua", game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/"..u.."/refs/heads/main/main.lua", true)) writefile(d.."/version.cfg", game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/"..u.."/refs/heads/main/version.txt"):gsub("%s+", "")) end
+loadstring(readfile(d.."/main.lua"))()
